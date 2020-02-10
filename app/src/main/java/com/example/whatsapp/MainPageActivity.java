63d53a18b8317b072f1cea61abf0215f5ed32666
AishwaryaUpadhyaya <ashyu136@gmail.com>
2020-02-11 00:48:12 +0530

    Permission to get contact list, done.

--- app/src/main/java/com/example/whatsapp/MainPageActivity.java
+++ app/src/main/java/com/example/whatsapp/MainPageActivity.java
@@ -1,8 +1,11 @@
 package com.example.whatsapp;
 
+import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,5 +39,13 @@ public class MainPageActivity extends AppCompatActivity {
             }
         });
 
+        getPermissions();
+
+    }
+
+    private void getPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, 1);
+        }
     }
 }
